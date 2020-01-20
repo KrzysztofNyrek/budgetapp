70659b761c0f1fd3a38e58199bd649d2c0c34f39
Krzysztof Nyrek <nitrobenzen@gmail.com>
2020-01-20 20:36:18 +0100

    Add comment to code

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+//BUDGET CONTROLLER
 var budgetController = (function(){
 
 })();
 
+//UI CONTROLLER
 var UIController = (function(){
 
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
 })(budgetController, UIController);
