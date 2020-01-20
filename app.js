b4880e4de2c06e2fd62337ede8ad1ed32ec1b0a9
Krzysztof Nyrek <nitrobenzen@gmail.com>
2020-01-20 20:45:40 +0100

    Add event listener to add button

--- app.js
+++ app.js
@@ -11,4 +11,11 @@ var UIController = (function(){
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+  document.querySelector('.add__btn').addEventListener('click', function(){
+    // 1. Get the filed input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate  the budget
+    // 5. Display the budget to the UI
+  });
 })(budgetController, UIController);
