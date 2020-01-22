35b1787b9273164390cb1e56be5723561bfdce6f
Krzysztof Nyrek <nitrobenzen@gmail.com>
2020-01-22 12:01:17 +0100

    Added function that clear fields after add

--- app.js
+++ app.js
@@ -95,12 +95,23 @@ var UIController = (function(){
       
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
-  },
+    },
+    
+    clearFields: function(){
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+      fieldsArr = Array.prototype.slice.call(fields); //allow get back array from list
+      //loops for all array elements and set them empty
+      fieldsArr.forEach(function(current, index, array){
+        current.value = "";
+      });
+    },
     
     getDOMstrings: function(){
       return DOMstrings;
     }
-  };
+  }
 })();
 
 // GLOBAL APP CONTROLLER
@@ -130,8 +141,11 @@ var controller = (function(budgetCtrl, UICtrl){
 
     // 3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
+
+    // 4. Clear fields
+    UICtrl.clearFields();
     // 4. Calculate  the budget
-    // 5. Display the budget to the UI
+    // 6. Display the budget to the UI
   };
 
   //function allow us to start aplication
