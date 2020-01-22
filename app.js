1bfcb0334331bb2f723993b78f32aab4da33e30b
Krzysztof Nyrek <nitrobenzen@gmail.com>
2020-01-22 16:25:25 +0100

    feat. display actual budget value

--- app.js
+++ app.js
@@ -89,7 +89,11 @@ var UIController = (function(){
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   };
 
   //Set below value public, that other parts of code have access to this value.
@@ -135,7 +139,17 @@ var UIController = (function(){
       });
       fieldsArr[0].focus();
     },
-    
+    displayBudget: function(obj){
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      
+      if (obj.percentage > 0){
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      }else{
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+    },
     getDOMstrings: function(){
       return DOMstrings;
     }
@@ -164,7 +178,7 @@ var controller = (function(budgetCtrl, UICtrl){
     // 2. Return the Budget
     var budget = budgetCtrl.getBudget();
     // 3. Display the budget to the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function(){
@@ -193,6 +207,12 @@ var controller = (function(budgetCtrl, UICtrl){
   //function allow us to start aplication
   return {
     init: function(){
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
