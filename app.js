f66fd4b1c1a8dbd3367c646c447e9ddf287d672b
Krzysztof Nyrek <nitrobenzen@gmail.com>
2020-01-23 16:40:16 +0100

    Feat: display actual date of budget

--- app.js
+++ app.js
@@ -127,7 +127,8 @@ var UIController = (function(){
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
   };
   //Formating number in budget items
   var formatNumber = function(num, type){
@@ -222,6 +223,14 @@ var UIController = (function(){
         
       });
     },
+    displayMonth: function(){
+      var now, year, month, months;
+      now = new Date();
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = now.getMonth();
+      year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
     getDOMstrings: function(){
       return DOMstrings;
     }
@@ -310,6 +319,7 @@ var controller = (function(budgetCtrl, UICtrl){
   //function allow us to start aplication
   return {
     init: function(){
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
